30660bd0f519f7a4e210b8376c2415dfd1e0b7b6
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-07-04 17:46:48 -0500

    Update chapter9ex02.c

--- chapter9ex02.c
+++ chapter9ex02.c
@@ -1,18 +1,9 @@
-
 #include <stdio.h>
 #include <string.h>
 
-//Prototipo de la funcion "compare" 
 int compare (char s[100]);
 void mayorQue(int a, int b);
-
 char line[100];
-
-/*Funcion "compare" te va a devolver un valor entero, que le puedes asignar a una variable en la funcion main
-Recuerda que existen diferentes tipos de datos en las funciones asi como en las variables, depende de que valor quieras devolver o 
-Almacenar en tu variable
-En este caso, queremos devolver un valor de tipo entero, por lo tanto tenemos que declarar nuestra funcion como entero
-y tenemos que devolverlo con la palabra reservada "return" seguido por la variable del tipo de la funcion que queremos devolver */
 int compare (char s[100]){
 int count, i;
 for (i = 0;s[i] != '\0';i++) {
@@ -29,12 +20,11 @@ void mayorQue(int a, int b){
 	}else if(b > a){
 		printf("El segundo string es el mas grande");
 	}else if (a == b){
-		printf("Los dos string son iguales");
+		printf("las dos cadenas son iguales");
 	}
 	
 	
 }
-
 
 int main(void) {
 
@@ -49,8 +39,7 @@ fgets(line, sizeof(line), stdin);
 sscanf(line,"%[^\n]s", cad2);
 
 resultado1 = compare (cad1);
-resultado2 = compare(cad2); /*En esta variable guardamos el dato que estamos devolviendo de la funcion "compare" y la guardamos en 
-en la variable result, recuerda que deben ser el mismo tipo de dato*/
+resultado2 = compare(cad2); 
 printf("%d \n%d\n",resultado1,resultado2);
 
 mayorQue(resultado1,resultado2);
