77f8a911501804d1729e7c8c94175e0fbb368afd
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-07-04 20:43:15 -0500

    Update chapter9ex01.c

--- chapter9ex01.c
+++ chapter9ex01.c
@@ -1,3 +1,4 @@
+/*Write a procedure that counts the number of words in a string. (Your documentation should describe exactly how you define a word.) Write a program to test your new procedure.*/
 #include <stdio.h>
 #include <string.h>
 
