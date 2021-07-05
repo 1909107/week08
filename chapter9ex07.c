eb801d76bcb326280409ac1b7fb475faa93e3fff
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-07-04 19:31:24 -0500

    Update chapter9ex07.c

--- chapter9ex07.c
+++ chapter9ex07.c
@@ -1,32 +1,20 @@
 /*chapter 9 exercise that lets me know if a person is right for you or not */
 #include <stdio.h>
 #include <string.h>
-int compare (char s[100]);
-void mayorQue(int a, int b);
-char line[100];
-int compare (char s[100]){
-int count, i;
-for (i = 0;s[i] != '\0';i++) {
+void funcion(steep1){ 
+{
+  char line[100];
+char s[100];
+char l[100];
+int count1, count2, i,j;
+printf("ingresa tu nombre completo con espacios:\n");
+fgets(line, sizeof(line), stdin);
+sscanf(line,"%[^\n]s", s);
+for (i = 0;s[i] != '\0';i++)
+{
 if (s[i] == ' ' && s[i+1] != ' ')
-count++; }
-
-return count;
-}
-
-void mayorQue(int a, int b){
-	
-	if(a > b){
-		printf("ingresa tu nombre completo con espacios");
-	}else if(b > a){
-		printf("Ingresa el nombre de la persona que amas con espacios");
-	}
-	
-}
-
-int main(void) {
-
-char cad1[100],cad2[100];
-
+count1++;
+}printf("Ingresa el nombre de la persona que amas con espacios:\n");
 fgets(line, sizeof(line), stdin);
 sscanf(line,"%[^\n]l", l);
 for (j = 0;l[j] != '\0';j++)
@@ -39,8 +27,10 @@ printf("esa persona es la indicada");
 else{
 printf("sal de ahi soldado");
 }
-return 0;
-mayorQue(resultado1,resultado2);
-
-return 0;
 }
+
+int main(){int steep1;
+funcion(steep1}
+return;
+}
+
